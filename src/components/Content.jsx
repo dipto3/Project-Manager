6a9e6600a32a274e81de426980bcbce015473c20
Piyal Guho Dipto <diptoguho4@gmail.com>
2024-10-23 02:20:11 +0600

    List Component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,8 @@
+import DoneList from "./category/DoneList";
+import OnProgressList from "./category/OnProgressList";
+import ReviseList from "./category/ReviseList";
+import ToDoList from "./category/ToDoList";
 import AddSvg from "./svg/AddSvg";
-import DeleteSvg from "./svg/DeleteSvg";
-import EditSvg from "./svg/EditSvg";
-import FilterSvg from "./svg/FilterSvg";
 
 export default function Content() {
   return (
@@ -18,132 +19,10 @@ export default function Content() {
         </div>
 
         <div className="-mx-2 mb-6 flex flex-wrap">
-          <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
-            <div className="rounded-lg bg-indigo-600 p-4">
-              <div className="mb-2 flex items-center justify-between">
-                <h3 className="text-lg font-semibold">To-Do (45)</h3>
-                <FilterSvg />
-              </div>
-              <div>
-                <div className="mb-4 rounded-lg bg-gray-800 p-4">
-                  <div className="flex justify-between">
-                    <h4 className="mb-2 flex-1 font-semibold text-indigo-500">
-                      Content Writer
-                    </h4>
-
-                    <div className="flex gap-2">
-                      <button>
-                        <DeleteSvg />
-                      </button>
-                      <button>
-                        <EditSvg />
-                      </button>
-                    </div>
-                  </div>
-                  <p className="mb-2 text-sm text-zinc-200">
-                    Prepare proctor for client meeting
-                  </p>
-
-                  <p className="mt-6 text-xs text-zinc-400">
-                    February 20, 2024
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
-            <div className="rounded-lg bg-yellow-500 p-4">
-              <div className="mb-2 flex items-center justify-between">
-                <h3 className="text-lg font-semibold">On Progress (45)</h3>
-                <FilterSvg />
-              </div>
-
-              <div className="mb-4 rounded-lg bg-gray-800 p-4">
-                <div className="flex justify-between">
-                  <h4 className="mb-2 flex-1 font-semibold text-yellow-500">
-                    Content Writer
-                  </h4>
-                  <div className="flex gap-2">
-                    <button>
-                      <DeleteSvg />
-                    </button>
-                    <button>
-                      <EditSvg />
-                    </button>
-                  </div>
-                </div>
-                <p className="mb-2 text-sm text-zinc-200">
-                  Prepare proctor for client meeting
-                </p>
-
-                <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
-            <div className="rounded-lg bg-teal-500 p-4">
-              <div className="mb-2 flex items-center justify-between">
-                <h3 className="text-lg font-semibold">Done (1)</h3>
-                <FilterSvg />
-              </div>
-
-              <div>
-                <div className="mb-4 rounded-lg bg-gray-800 p-4">
-                  <div className="flex justify-between">
-                    <h4 className="mb-2 font-semibold text-teal-500">
-                      Content Writer
-                    </h4>
-                    <div className="flex gap-2">
-                      <button>
-                        <DeleteSvg />
-                      </button>
-                      <button>
-                        <EditSvg />
-                      </button>
-                    </div>
-                  </div>
-                  <p className="mb-2 text-sm text-zinc-200">
-                    Make Promotional Ads for Instagram {`fasto's`}
-                  </p>
-
-                  <p className="mt-6 text-xs text-zinc-400">
-                    February 20, 2024
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
-            <div className="rounded-lg bg-rose-500 p-4">
-              <div className="mb-2 flex items-center justify-between">
-                <h3 className="text-lg font-semibold">Revise (1)</h3>
-                <FilterSvg />
-              </div>
-              <div className="mb-4 rounded-lg bg-gray-800 p-4">
-                <div className="flex justify-between">
-                  <h4 className="mb-2 font-semibold text-rose-500">
-                    Content Writer
-                  </h4>
-                  <div className="flex gap-2">
-                    <button>
-                      <DeleteSvg />
-                    </button>
-                    <button>
-                      <EditSvg />
-                    </button>
-                  </div>
-                </div>
-                <p className="mb-2 text-sm text-zinc-200">
-                  Make Promotional Ads for Instagram {`fasto's`}
-                </p>
-
-                <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
-              </div>
-            </div>
-          </div>
+          <ToDoList />
+          <OnProgressList />
+          <DoneList />
+          <ReviseList />
         </div>
       </div>
     </>
---
+++ src/components/category/ReviseList.jsx
@@ -0,0 +1,37 @@
+import DeleteSvg from "../svg/DeleteSvg";
+import EditSvg from "../svg/EditSvg";
+import FilterSvg from "../svg/FilterSvg";
+export default function ReviseList() {
+  return (
+    <>
+      <div className="mb-4 w-full px-2 sm:w-1/2 md:w-1/4">
+        <div className="rounded-lg bg-rose-500 p-4">
+          <div className="mb-2 flex items-center justify-between">
+            <h3 className="text-lg font-semibold">Revise (1)</h3>
+            <FilterSvg />
+          </div>
+          <div className="mb-4 rounded-lg bg-gray-800 p-4">
+            <div className="flex justify-between">
+              <h4 className="mb-2 font-semibold text-rose-500">
+                Content Writer
+              </h4>
+              <div className="flex gap-2">
+                <button>
+                  <DeleteSvg />
+                </button>
+                <button>
+                  <EditSvg />
+                </button>
+              </div>
+            </div>
+            <p className="mb-2 text-sm text-zinc-200">
+              Make Promotional Ads for Instagram {`fasto's`}
+            </p>
+
+            <p className="mt-6 text-xs text-zinc-400">February 20, 2024</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
